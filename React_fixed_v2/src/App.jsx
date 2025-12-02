8e95982702f1d7d69bfe547e1c068307fe3c1bec
BasmalaSalah88 <168294790+BasmalaSalah88@users.noreply.github.com>
2025-12-02 23:35:39 +0200

    Add new page imports to App component

--- React_fixed_v2/src/App.jsx
+++ React_fixed_v2/src/App.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Details from "./pages/Details";
+import Settings from "./pages/Settings";
+import Transactions from "./pages/Transactions";
+
 
 
 
