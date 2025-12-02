cc2fa255f1e5190e650272fb97f1e60505a5542a
7odaZc <s-mahmoud.elzayat@zewailcity.edu.eg>
2025-12-02 20:02:11 +0200

    DashBoard

---
+++ React_fixed_v2/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+
+
+export default function App() {
+  return (
+    // this wrapper is redundant for background now, but keeps layout consistent
+    <div className="min-h-screen w-full bg-transparent">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
